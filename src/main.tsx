e17cae237325a0fb897de2e0b278ca1c977a47d7
Sanderchriss <sanderchriss@gmail.com>
2024-01-26 13:06:21 +0100

    Open layers and CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Application } from "./application";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>artig</h1>);
+root.render(<Application />);
