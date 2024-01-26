ef581e833742d013dd83aaf547c28dea6abac77b
Sanderchriss <sanderchriss@gmail.com>
2024-01-26 14:59:11 +0100

    Checker Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Application } from "./application";
+//import { Application } from "./application";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<Application />);
+root.render(<h1>wow, togaaa</h1>);
