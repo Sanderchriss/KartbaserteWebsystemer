42e921e27766865ee57b840b1d8f48cdbe56accc
Sanderchriss <sanderchriss@gmail.com>
2024-01-31 12:23:56 +0100

    gh-page test

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 //import { Application } from "./application";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>wow, togaaa</h1>);
+root.render(<h1>wow, tog</h1>);
